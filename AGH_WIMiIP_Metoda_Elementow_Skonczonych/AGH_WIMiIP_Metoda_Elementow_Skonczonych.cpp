9cca366bcb4d4665231a229b608476e33ee9b49c
Adamczyk Piotr <adamczyk.piter@outlook.com>
2019-11-21 07:01:25 +0100

    Added Numeric.h with gaussian elimination and gaussian quadrature methods

--- AGH_WIMiIP_Metoda_Elementow_Skonczonych/AGH_WIMiIP_Metoda_Elementow_Skonczonych.cpp
+++ AGH_WIMiIP_Metoda_Elementow_Skonczonych/AGH_WIMiIP_Metoda_Elementow_Skonczonych.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
- 
+#include <iterator>
+
 #include "Grid.h"
+#include "Numeric.h"
 
 
 int main() {
@@ -21,5 +23,28 @@ int main() {
 	std::cout << elements.at(0).getNode(1);
 	std::cout << elements.at(0).getNode(2);
 	std::cout << elements.at(0).getNode(3);
+
+	/*
+
+	2x + 5y + 3z = 5
+	4x + 2y + 5z = 4
+	3x + 8y + 4z = 9
+	
+	*/
+
+	std::vector<std::vector<double>> matrix = { {2,5,3,	5}, {4,2,5,	4}, {3,8,4,	9} };
+	std::vector<double> vec = Numeric::gaussianElimination(&matrix);
+	for (int i = 0; i < vec.size(); i++) {
+		std::cout << "x" << i+1 << " = " << vec[i] << "\n";
+	}
+	std::cout << "\n";
+
+	std::vector<double> xi = { 0.,5.,5.,0. };
+	std::vector<double> yi = { 0.,0.,5.,5. };
+	std::vector<double> weight = { 1.,1. };
+	std::vector<double> point = { -0.5773502692, 0.5773502692 };
+
+	double area = Numeric::gaussianQuadrature(xi, yi, weight, point);
+	std::cout << "Area: " << area << "\n";
 }
 
