6fc579a07422d38813d616dc6e2f873dcfefcfdb
Adamczyk Piotr <adamczyk.piter@outlook.com>
2020-01-12 14:09:18 +0100

    Fixed Jacobian variables naming scheme

--- AGH_WIMiIP_Metoda_Elementow_Skonczonych/Jacobian.cpp
+++ AGH_WIMiIP_Metoda_Elementow_Skonczonych/Jacobian.cpp
@@ -4,40 +4,40 @@
 
 Jacobian::Jacobian(std::array<double, 4> x, std::array<double, 4> y, int point, UniversalElement element) {
 
-	double dxDksi =
+	double dX_dKsi =
 		  x[0] * element.getKsiValue(point, 0)
 		+ x[1] * element.getKsiValue(point, 1)
 		+ x[2] * element.getKsiValue(point, 2)
 		+ x[3] * element.getKsiValue(point, 3);
-	double dyDksi =
+	double dY_dKsi =
 		  y[0] * element.getKsiValue(point, 0)
 		+ y[1] * element.getKsiValue(point, 1)
 		+ y[2] * element.getKsiValue(point, 2)
 		+ y[3] * element.getKsiValue(point, 3);
-	double dxDeta =
+	double dX_dEta =
 		  x[0] * element.getEtaValue(point, 0)
 		+ x[1] * element.getEtaValue(point, 1)
 		+ x[2] * element.getEtaValue(point, 2)
 		+ x[3] * element.getEtaValue(point, 3);
-	double dyDeta =
+	double dY_dEta =
 		  y[0] * element.getEtaValue(point, 0)
 		+ y[1] * element.getEtaValue(point, 1)
 		+ y[2] * element.getEtaValue(point, 2)
 		+ y[3] * element.getEtaValue(point, 3);
 
 	//Setting jacobian values
-	m_data[0][0] = dxDksi;
-	m_data[0][1] = dyDksi;
-	m_data[1][0] = dxDeta;
-	m_data[1][1] = dyDeta;
+	m_data[0][0] = dX_dKsi;
+	m_data[0][1] = dY_dKsi;
+	m_data[1][0] = dX_dEta;
+	m_data[1][1] = dY_dEta;
 
 	//Calculating determinant
-	m_determinant  = m_data[0][0] * m_data[1][1];
-	m_determinant -= m_data[0][1] * m_data[1][0];
+	m_determinant  = dX_dKsi * dY_dEta;
+	m_determinant -= dY_dKsi * dX_dEta;
 
 	//Calculating inverted jacobian values
-	m_invertedData[0][0] =		m_data[1][1] / m_determinant;
-	m_invertedData[0][1] = -1 * m_data[0][1] / m_determinant;
-	m_invertedData[1][0] = -1 * m_data[1][0] / m_determinant;
-	m_invertedData[1][1] =		m_data[0][0] / m_determinant;
+	m_invertedData[0][0] =		dY_dEta / m_determinant;
+	m_invertedData[0][1] = -1 * dY_dKsi / m_determinant;
+	m_invertedData[1][0] = -1 * dX_dEta / m_determinant;
+	m_invertedData[1][1] =		dX_dKsi / m_determinant;
 }
