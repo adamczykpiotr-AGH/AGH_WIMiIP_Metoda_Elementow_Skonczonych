2b22d55f37968e1626b26098a7c7e5c939f44f42
Adamczyk Piotr <adamczyk.piter@outlook.com>
2020-01-25 19:55:47 +0100

    Removed unused Numeric class

--- AGH_WIMiIP_Metoda_Elementow_Skonczonych/Numeric.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-class Numeric {
-public:
-	static double gaussianQuadrature(const std::vector<double>& xi, const std::vector<double>& yi, const std::vector<double>& weight, const std::vector<double>& point);
-	static std::vector<double> gaussianElimination(std::vector<std::vector<double>>* matrixPtr);
-};
-
